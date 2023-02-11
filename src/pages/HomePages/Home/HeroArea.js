88f9eea0de4b221c2fe9ce0df120c42509f51d99
mdansarulislam641 <mdansarulislam641.com>
2023-02-11 10:02:11 +0600

    live link set in readme file

--- src/pages/HomePages/Home/HeroArea.js
+++ src/pages/HomePages/Home/HeroArea.js
@@ -7,6 +7,8 @@ const HeroArea = () => {
             <img className='w-full' src='https://i.ibb.co/28Mrtfn/images-1.png' alt="" />
             <img className='w-full' src='https://i.ibb.co/cv5cDDq/images-4.png' alt="" />
 
+            <div className='bg-gray-600'></div>
+
             {/* banner text */}
             <div className='absolute max-w-[800px] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
             <h2 className='text-white text-[96px] text-center leading-[110px] font-bold'>Sports Clothing & Fitness Equipment Shopify Theme</h2>
